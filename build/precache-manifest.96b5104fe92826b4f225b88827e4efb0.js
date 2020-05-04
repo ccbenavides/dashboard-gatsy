51c5589dde66f15aa5343f5701c2370e2de1999a
ccbenavi <ccbenavides19@gmail.com>
2020-05-04 01:43:54 -0500

    __

---
+++ build/precache-manifest.96b5104fe92826b4f225b88827e4efb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25cff3d0a8a9fd5eafdd8d400b28e5a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b0f3707ff301f8dfd1e",
+    "url": "/static/css/main.17b1f633.chunk.css"
+  },
+  {
+    "revision": "077efd749e42df0b9f3d",
+    "url": "/static/js/2.d0a16de7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d0a16de7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b0f3707ff301f8dfd1e",
+    "url": "/static/js/main.48047489.chunk.js"
+  },
+  {
+    "revision": "3fd61b8e61cbba6514e7",
+    "url": "/static/js/runtime-main.10f400ce.js"
+  }
+]);
